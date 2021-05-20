c0d9c9201d260ca7bde8b78daf5c90ed681fd99c
vanessabach <vanessabach.r@gmail.com>
2021-05-20 19:38:18 -0300

    global styles done

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,16 @@ import  { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f0f2f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+        
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
 
   *{
@@ -25,4 +34,23 @@ export const GlobalStyle = createGlobalStyle`
     background: var(--background);
     --webkit-font-smoothing: antialiased;
   }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
 `
